c6a166532b8e2272784c4e14306b4a5f8e502cda
lmalyszko <lmalyszko@grupazpr.pl>
2020-03-28 16:43:41 +0100

    rename

--- query/tests/test_query.py
+++ query/tests/test_query.py
@@ -7,19 +7,18 @@ from query.query_preprocessor import QueryPreprocessor, InvalidVariablesExceptio
 class TestQueryPreprocessor(TestCase):
     def setUp(self) -> None:
         self.query_preprocessor = QueryPreprocessor()
-        self.expected_variables = "stmt s, s1; assign a, a1, a2; while w; if ifstat; procedure p; variable v; constant c; prog_line n, n1, n2;"
-        self.expected_query = "Select s such that Modifies(s,'x')"
+        self.variables = "stmt s, s1; assign a, a1, a2; while w; variable v; constant c; prog_line n, n1, n2;"
+        self.query = "Select s such that Modifies(s,'x')"
 
     def test_returns_get_input_result(self):
-        input_values = [self.expected_variables, self.expected_query]
+        input_values = [self.variables, self.query]
 
         with patch('builtins.input', side_effect=input_values):
-            self.assertEqual(self.query_preprocessor.get_input(), (self.expected_variables, self.expected_query))
+            self.assertEqual(self.query_preprocessor.get_input(), (self.variables, self.query))
 
     def test_raise_invalid_variables_exception_when_no_semicolon_at_the_end(self):
-        input_values = [self.expected_variables[:-1], self.expected_query]
+        input_values = [self.variables[:-1], self.query]
 
         with patch('builtins.input', side_effect=input_values):
             with self.assertRaises(InvalidVariablesException):
                 self.query_preprocessor.get_input()
-
