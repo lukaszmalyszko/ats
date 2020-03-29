ac0b08b71dcd0d02ad9014db43776d9f8c15b375
lmalyszko <lmalyszko@grupazpr.pl>
2020-03-29 19:07:01 +0200

    refactor declarations

--- query/variables_validator/declarations.py
+++ query/variables_validator/declarations.py
@@ -2,17 +2,11 @@ class Declarations:
     def __init__(self):
         self.entities = {}
 
-    def check_if_contains_variable(self, var_name):
+    def check_if_contains_variable(self, var_name, var_type=None):
         for var_list in self.entities.values():
             for var in var_list:
                 if var_name == var.name:
+                    if var_type:
+                        return var.type == var_type
                     return True
         return False
-
-    def check_if_contains_stmt(self, stmt_name):
-        # TODO
-        return True
-
-    def check_if_contains_ref(self, ref_name):
-        # TODO
-        return True
