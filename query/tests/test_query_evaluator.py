a2b4e599fc87b739d6d6a597074a6064fe2bd0b4
aziuzia <aziuzia@grupazpr.pl>
2020-06-06 21:50:39 +0200

    fix testow

--- query/tests/test_query_evaluator.py
+++ query/tests/test_query_evaluator.py
@@ -9,14 +9,14 @@ class TestQueryEvaluator(PkbTestCase):
         super().setUp()
         self.expected_result = [
             "1, 3, 6, 8, 10, 12, 16, 17, 19, 20, 22, 23, 25, 29, 32, 34, 35, 36, 37",
-            "3, 16, 19, 32, 37",
+            "2, 14, 17, 26, 29",
         ]
         self.query_evaluator = QueryEvaluator(self.pkb)
 
     def test_select_statement_that_modifies_x(self):
         # Arrange
         variables = "stmt s;"
-        query = "Select s such that Modifies(s,'x')"
+        query = 'Select s such that Modifies(s,"x")'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
@@ -27,7 +27,7 @@ class TestQueryEvaluator(PkbTestCase):
     def test_select_assign_that_modifies_x(self):
         # Arrange
         variables = "assign a;"
-        query = "Select a such that Modifies(a,'x')"
+        query = 'Select a such that Modifies(a,"x")'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
@@ -38,7 +38,7 @@ class TestQueryEvaluator(PkbTestCase):
     def test_select_assign_that_modifies_v_with_var_name(self):
         # Arrange
         variables = "assign a; variable v;"
-        query = "Select a such that Modifies (a,v) with v.varName='x'"
+        query = 'Select a such that Modifies (a,v) with v.varName="x"'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
@@ -57,24 +57,24 @@ class TestQueryEvaluator(PkbTestCase):
             result = self.query_evaluator.get_result()
             self.assertEqual(
                 result,
-                "3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 17, 18, 19, 20, 25, 26, 27, 32",
+                "2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 15, 16, 17, 18, 21, 22, 23, 26",
             )
 
     def test_select_assign_that_is_followed_by_prog_line(self):
         # Arrange
         variables = "assign a;"
-        query = "Select a such that Follows (14, a)"
+        query = "Select a such that Follows (15, a)"
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "13")
+            self.assertEqual(result, "14")
 
     def test_select_assign_that_is_followed_by_prog_line_with_no_result(self):
         # Arrange
         variables = "assign a;"
-        query = "Select a such that Follows (13, a)"
+        query = "Select a such that Follows (12, a)"
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
@@ -85,13 +85,13 @@ class TestQueryEvaluator(PkbTestCase):
     def test_select_assign_that_follows_prog_line(self):
         # Arrange
         variables = "assign a;"
-        query = "Select a such that Follows (a, 13)"
+        query = "Select a such that Follows (a, 12)"
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "14")
+            self.assertEqual(result, "13")
 
     def test_select_statement_that_follows_with_prog_line(self):
         # Arrange
@@ -113,40 +113,40 @@ class TestQueryEvaluator(PkbTestCase):
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "10, 12, 23, 30, 35, 36")
+            self.assertEqual(result, "9, 11, 19, 24, 27, 28")
 
     def test_select_statement_that_is_using_with_var_name(self):
         # Arrange
         variables = "stmt s1; variable v;"
-        query = "Select s1 such that Uses (s1, v) with v.varName='x'"
+        query = 'Select s1 such that Uses (s1, v) with v.varName="x"'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "4, 5, 7, 9, 11, 24, 31, 32, 39")
+            self.assertEqual(result, "3, 4, 6, 8, 10, 20, 25, 26, 30")
 
     def test_select_statement_that_modifies_and_uses(self):
         # Arrange
         variables = "stmt s1; variable v;"
-        query = "Select s1 such that Modifies(s1,'x') and Uses(s1,'y')"
+        query = 'Select s1 such that Modifies(s1,"x") and Uses(s1,"y")'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "3, 16, 19, 32, 37")
+            self.assertEqual(result, "2, 14, 17, 26, 29")
 
     def test_select_statements_that_modifies(self):
         # Arrange
         variables = "stmt s1; variable v;"
-        query = "Select <s1, v> such that Modifies(s1,v) with v.varName='x'"
+        query = 'Select <s1, v> such that Modifies(s1,v) with v.varName="x"'
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "3 x, 16 x, 19 x, 32 x, 37 x")
+            self.assertEqual(result, "2 x, 14 x, 17 x, 26 x, 29 x")
 
     def test_select_statement_that_is_parent_star(self):
         # Arrange
@@ -158,8 +158,8 @@ class TestQueryEvaluator(PkbTestCase):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
             self.assertEqual(result,
-                             "10 11, 10 12, 10 13, 10 14, 10 16, 10 17, 10 18, 10 19, 12 13, 12 14, 12 16, 12 17, "
-                             "12 18, 23 24, 23 25, 23 26, 30 31, 35 36, 35 37, 35 39, 36 37, 36 39")
+                             "9 10, 9 11, 9 12, 9 13, 9 14, 9 15, 9 16, 9 17, 11 12, 11 13, 11 14, 11 15, 11 16, "
+                             "19 20, 19 21, 19 22, 24 25, 27 28, 27 29, 27 30, 28 29, 28 30")
 
     def test_select_statement_that_is_parent_star_with(self):
         # Arrange
@@ -170,18 +170,18 @@ class TestQueryEvaluator(PkbTestCase):
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "10 14, 12 14")
+            self.assertEqual(result, "9 14, 11 14")
 
     def test_select_statement_that_is_parent_star_with_first_param_condition(self):
         # Arrange
         variables = "stmt s1, s2;"
-        query = "Select <s1, s2> such that Parent* (s1, s2) with s1.stmt#= 10"
+        query = "Select <s1, s2> such that Parent* (s1, s2) with s1.stmt#= 9"
         input_values = [variables, query]
         # Act & Assert
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "10 11, 10 12, 10 13, 10 14, 10 16, 10 17, 10 18, 10 19")
+            self.assertEqual(result, "9 10, 9 11, 9 12, 9 13, 9 14, 9 15, 9 16, 9 17")
 
     def test_select_statement_that_follows_star(self):
         # Arrange
@@ -193,10 +193,10 @@ class TestQueryEvaluator(PkbTestCase):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
             self.assertEqual(result,
-                             "3 2, 4 2, 4 3, 5 2, 5 3, 5 4, 6 2, 6 3, 6 4, 6 5, 7 2, 7 3, 7 4, 7 5, 7 6, 8 2, 8 3, "
-                             "8 4, 8 5, 8 6, 8 7, 9 2, 9 3, 9 4, 9 5, 9 6, 9 7, 9 8, 10 2, 10 3, 10 4, 10 5, 10 6, "
-                             "10 7, 10 8, 10 9, 12 11, 14 13, 17 16, 18 16, 18 17, 19 11, 19 12, 20 2, 20 3, 20 4, "
-                             "20 5, 20 6, 20 7, 20 8, 20 9, 20 10, 25 24, 26 24, 26 25, 27 23, 32 30")
+                             "2 1, 3 1, 3 2, 4 1, 4 2, 4 3, 5 1, 5 2, 5 3, 5 4, 6 1, 6 2, 6 3, 6 4, 6 5, 7 1, 7 2, "
+                             "7 3, 7 4, 7 5, 7 6, 8 1, 8 2, 8 3, 8 4, 8 5, 8 6, 8 7, 9 1, 9 2, 9 3, 9 4, 9 5, 9 6, "
+                             "9 7, 9 8, 11 10, 13 12, 15 14, 16 14, 16 15, 17 10, 17 11, 18 1, 18 2, 18 3, 18 4, "
+                             "18 5, 18 6, 18 7, 18 8, 18 9, 21 20, 22 20, 22 21, 23 19, 26 24")
 
     def test_select_line_that_calls(self):
         # Arrange
@@ -207,4 +207,4 @@ class TestQueryEvaluator(PkbTestCase):
         with patch("builtins.input", side_effect=input_values):
             self.query_evaluator.load()
             result = self.query_evaluator.get_result()
-            self.assertEqual(result, "6, 8, 17, 20, 25")
+            self.assertEqual(result, "5, 7, 15, 18, 21")
--- query/tests/test_query_validation.py
+++ query/tests/test_query_validation.py
@@ -13,10 +13,10 @@ class TestQueryValidation(PkbTestCase):
         super().setUp()
         self.query_preprocessor = QueryPreprocessor(self.pkb)
         self.variables = "stmt s, s1; assign a, a1, a2; while w; variable v; constant c; prog_line n, n1, n2;"
-        self.query = "Select s such that Modifies(s,'x')"
+        self.query = 'Select s such that Modifies(s,"x")'
 
     def test_raise_invalid_query_exception_when_no_variable_after_select(self):
-        self.query = "Select such that Modifies(s,'x')"
+        self.query = 'Select such that Modifies(s,"x")'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -24,7 +24,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_no_select_at_the_beginning(self):
-        self.query = "s such that Modifies(s,'x')"
+        self.query = 's such that Modifies(s,"x")'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -32,7 +32,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_variable_is_key_word(self):
-        self.query = "Select Select such that Modifies(s,'x')"
+        self.query = 'Select Select such that Modifies(s,"x")'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -40,7 +40,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_no_such_that_after_variable(self):
-        self.query = "Select s Modifies(s, 'x')"
+        self.query = 'Select s Modifies(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -49,7 +49,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_no_that_after_such(self):
-        self.query = "Select s such Modifies(s, 'x')"
+        self.query = 'Select s such Modifies(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -58,7 +58,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_rel_has_no_closing_brackets(self):
-        self.query = "Select s such that Modifies(s, 'x'"
+        self.query = 'Select s such that Modifies(s, "x"'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -82,7 +82,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_with_is_incorrect(self):
-        self.query = "Select s such that Modifies(s, 'x') wiiith s.a=10"
+        self.query = 'Select s such that Modifies(s, "x") wiiith s.a=10'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -90,7 +90,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_with_has_no_condition(self):
-        self.query = "Select s such that Modifies(s, 'x') with"
+        self.query = 'Select s such that Modifies(s, "x") with'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -98,7 +98,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_relation_param_is_incorrect(self):
-        self.query = "Select s such that Modifies(b, 'x')"
+        self.query = 'Select s such that Modifies(b, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -107,7 +107,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_select_param_is_incorrect(self):
-        self.query = "Select b such that Modifies(s, 'x')"
+        self.query = 'Select b such that Modifies(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -125,7 +125,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_parent_params_are_incorrect(self):
-        self.query = "Select s such that Parent(s, 'x')"
+        self.query = 'Select s such that Parent(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -134,7 +134,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_parent_star_params_are_incorrect(self):
-        self.query = "Select s such that Parent*(s, 'x')"
+        self.query = 'Select s such that Parent*(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -143,7 +143,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_follows_params_are_incorrect(self):
-        self.query = "Select s such that Follows(s, 'x')"
+        self.query = 'Select s such that Follows(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -152,7 +152,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_follows_star_params_are_incorrect(self):
-        self.query = "Select s such that Follows*(s, 'x')"
+        self.query = 'Select s such that Follows*(s, "x")'
 
         input_values = [self.variables, self.query]
 
@@ -161,7 +161,7 @@ class TestQueryValidation(PkbTestCase):
                 self.query_preprocessor.get_input()
 
     def test_raise_invalid_query_exception_when_condition_is_incorrect(self):
-        self.query = "Select s such that Modifies(s, 'x') with s.a"
+        self.query = 'Select s such that Modifies(s, "x") with s.a'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
@@ -191,7 +191,7 @@ class TestQueryValidation(PkbTestCase):
     def test_raise_invalid_query_exception_when_no_and_between_relations(
         self,
     ):
-        self.query = "Select s such that Modifies(s, v) Uses(s,'x')"
+        self.query = 'Select s such that Modifies(s, v) Uses(s,"x")'
         input_values = [self.variables, self.query]
 
         with patch("builtins.input", side_effect=input_values):
