63079c5b7a36a0f9ab5ae5ae5569baed3cc50373
aziuzia <aziuzia@grupazpr.pl>
2020-06-09 16:57:04 +0200

    fix message exception

--- query/query_parser/exceptions.py
+++ query/query_parser/exceptions.py
@@ -2,7 +2,13 @@ class InvalidQueryException(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return self.message
+
 
 class InvalidQueryParamException(Exception):
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return self.message
