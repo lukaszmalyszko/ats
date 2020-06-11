cafc58f0591dfc7aa4f12b56ed6af07348df6408
aziuzia <aziuzia@grupazpr.pl>
2020-06-11 18:32:30 +0200

    obsluga procedur

--- query/query_tree/tree_nodes.py
+++ query/query_tree/tree_nodes.py
@@ -16,7 +16,7 @@ MAP_CLASS_TO_GET_METHOD = {
     Assign: "get_assign_map",
     Variable: "get_variables_map",
     ProgLine: "get_nodes_map",
-    Procedure: "get_nodes_map",
+    Procedure: "get_procedures_map",
 }
 
 MAP_ATTR_NAME_TO_NODE_ATTRIBUTE = {"varName": "get_value", "stmt#": "get_line"}
@@ -337,7 +337,7 @@ class CallsNode(RelationNode):
             first_index, first_node = list(first_arg.items())[0]
             for second_arg in second_args:
                 second_index, second_node = list(second_arg.items())[0]
-                if pkb.isCalling(first_index, second_index):
+                if pkb.isCalling(first_index, second_node.get_value()):
                     self._first_arg_result.append({first_index: first_node})
                     self._second_arg_result.append({second_index: second_node})
         return {
